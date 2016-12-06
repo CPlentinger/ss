ef183ecf5e60d90cb192b1077e259ae159ecd36b
casper <casper@casper-Lenovo>
2016-12-06 12:05:04 +0100

    Fixed checkstyle Util file, excercise 4.2

--- ss/src/ss/week4/Util.java
+++ ss/src/ss/week4/Util.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Util {
-    public static <Element> List<Element> zip
-    (List<Element> l1, List<Element> l2) {
-        ArrayList<Element> RESULT = new ArrayList<Element>();
+    public static <Element> List<Element> zip(List<Element> l1, List<Element> l2) {
+        ArrayList<Element> result = new ArrayList<Element>();
         for (int i = 0; i<l1.size(); i++) {
-            RESULT.add(l1.get(i));
-            RESULT.add(l2.get(i));
+            result.add(l1.get(i));
+            result.add(l2.get(i));
         }
-        return RESULT;
+        return result;
     }    
 
     /**
@@ -20,10 +19,12 @@ public class Util {
      * @return -1, 0 or 1 if the argument is negative, 0 or positive
      */
     public static int signum(int i) {
-        if(i < 0)
+        if(i < 0) {
             return -1;
-        else if (i > 0)
+        } else if (i > 0) {
             return 1;
-        else return 0;
+        } else {
+        	return 0;
+        }
     }
 }
